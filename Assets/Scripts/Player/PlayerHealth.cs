726e496b7b81cef480d98150685a89831002faa9
Nathan Marques <nathan.marques176@gmail.com>
2024-08-11 07:10:39 -0300

    feat: enemy shooting

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class PlayerHeath : MonoBehaviour
+public class PlayerHealth : MonoBehaviour
 {
     private float health;
     private float lerpTimer;
