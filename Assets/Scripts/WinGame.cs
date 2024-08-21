e5912d35dc64fb2fc0d3416a90ff7e6996c75692
Nathan Marques <novadrake76@gmail.com>
2024-08-21 15:19:25 -0300

    feat: ending screen and final boss

---
+++ Assets/Scripts/WinGame.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinGame : MonoBehaviour
+{
+    //if enemy life is 0, player wins
+    public Enemy enemy;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (enemy.health <= 0)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+
+}
